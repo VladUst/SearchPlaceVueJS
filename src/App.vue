98de41308a920f53228286ee2f8b62ac3caff044
VladUs <vladustyugo@gmail.com>
2021-11-03 23:16:12 +0700

    create project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,86 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row gx-5">
+      <div class="col-4">
+        <div class="search">
+          <form class="search__location">
+            <input
+              type="text"
+              class="
+                form-control
+                text-muted
+                form-rounded
+                p-2
+                text-center
+                shadow-sm
+              "
+              placeholder="What City?"
+              autocomplete="off"
+            />
+            <button type="button" class="btn btn-outline-success">Find</button>
+          </form>
+        </div>
+      </div>
+      <div class="col">
+        <div class="variants">
+          <div class="variants__title">Варианты мест</div>
+          <div class="variants__buttons">
+            <button type="button" class="btn btn-outline-primary">But1</button>
+            <button type="button" class="btn btn-outline-primary">But2</button>
+            <button type="button" class="btn btn-outline-primary">But3</button>
+            <button type="button" class="btn btn-outline-primary">But4</button>
+            <button type="button" class="btn btn-outline-primary">But5</button>
+          </div>
+          <div class="variants__coords">
+            <div class="coord">lat:500 lon:700</div>
+            <div class="coord">lat:500 lon:700</div>
+            <div class="coord">lat:500 lon:700</div>
+            <div class="coord">lat:500 lon:700</div>
+            <div class="coord">lat:500 lon:700</div>
+          </div>
+        </div>
+      </div>
+      <div class="col-12">
+        <div class="places">
+          <div class="places__info">
+            <div class="name">Место 1</div>
+            <div class="descr">Здесь могла быть ваша реклама</div>
+            <div class="weather">Температура:</div>
+          </div>
+          <div class="places__info">
+            <div class="name">Место 2</div>
+            <div class="descr">Здесь могла быть ваша реклама</div>
+            <div class="weather">Температура:</div>
+          </div>
+          <div class="places__info">
+            <div class="name">Место 3</div>
+            <div class="descr">Здесь могла быть ваша реклама</div>
+            <div class="weather">Температура:</div>
+          </div>
+          <div class="places__info">
+            <div class="name">Место 4</div>
+            <div class="descr">Здесь могла быть ваша реклама</div>
+            <div class="weather">Температура:</div>
+          </div>
+          <div class="places__info">
+            <div class="name">Место 5</div>
+            <div class="descr">Здесь могла быть ваша реклама</div>
+            <div class="weather">Температура:</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/layouts/searching.scss";
+@import "./assets/layouts/variants.scss";
+@import "./assets/layouts/places-info.scss";
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/layouts/index.scss'
 
 createApp(App).mount('#app')
