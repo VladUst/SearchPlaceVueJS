81227115ab87b74a7c7f50c335e5f12bcbb5308b
VladUs <vladustyugo@gmail.com>
2021-11-05 14:30:29 +0700

    add data

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,83 @@
 <template>
-  <div class="container">
-    <div class="row gx-5">
-      <div class="col-4">
-        <div class="search">
-          <form class="search__location">
-            <input
-              type="text"
-              class="
-                form-control
-                text-muted
-                form-rounded
-                p-2
-                text-center
-                shadow-sm
-              "
-              placeholder="What City?"
-              autocomplete="off"
-            />
-            <button type="button" class="btn btn-outline-success">Find</button>
-          </form>
-        </div>
-      </div>
-      <div class="col">
-        <div class="variants">
-          <div class="variants__title">Варианты мест</div>
-          <div class="variants__buttons">
-            <button type="button" class="btn btn-outline-primary">But1</button>
-            <button type="button" class="btn btn-outline-primary">But2</button>
-            <button type="button" class="btn btn-outline-primary">But3</button>
-            <button type="button" class="btn btn-outline-primary">But4</button>
-            <button type="button" class="btn btn-outline-primary">But5</button>
-          </div>
-          <div class="variants__coords">
-            <div class="coord">lat:500 lon:700</div>
-            <div class="coord">lat:500 lon:700</div>
-            <div class="coord">lat:500 lon:700</div>
-            <div class="coord">lat:500 lon:700</div>
-            <div class="coord">lat:500 lon:700</div>
+  <div id="main">
+    <div class="container">
+      <div class="row gx-5">
+        <div class="col-4">
+          <div class="search">
+            <form class="search__location" @submit="getCoords">
+              <input
+                type="text"
+                class="
+                  form-control
+                  text-muted
+                  form-rounded
+                  p-2
+                  text-center
+                  shadow-sm
+                "
+                v-model="placeSearch"
+                placeholder="What City?"
+                autocomplete="off"
+              />
+              <button
+                type="button"
+                class="btn btn-outline-success"
+                @click="getCoords"
+              >
+                Find
+              </button>
+            </form>
           </div>
         </div>
-      </div>
-      <div class="col-12">
-        <div class="places">
-          <div class="places__info">
-            <div class="name">Место 1</div>
-            <div class="descr">Здесь могла быть ваша реклама</div>
-            <div class="weather">Температура:</div>
+
+        <div class="col">
+          <div class="variants">
+            <div class="variants__title">Варианты мест</div>
+            <ul class="variants__buttons">
+              <button
+                type="button"
+                class="btn btn-outline-primary"
+                v-for="(button, id) in buttonsList"
+                :key="id"
+              >
+                {{ button.name }}
+              </button>
+            </ul>
+            <ul class="variants__coords">
+              <li class="coord" v-for="(coor, id) in coordsList" :key="id">
+                {{ coor.lat }}<br />{{ coor.lon }}
+              </li>
+            </ul>
           </div>
-          <div class="places__info">
-            <div class="name">Место 2</div>
-            <div class="descr">Здесь могла быть ваша реклама</div>
-            <div class="weather">Температура:</div>
-          </div>
-          <div class="places__info">
-            <div class="name">Место 3</div>
-            <div class="descr">Здесь могла быть ваша реклама</div>
-            <div class="weather">Температура:</div>
-          </div>
-          <div class="places__info">
-            <div class="name">Место 4</div>
-            <div class="descr">Здесь могла быть ваша реклама</div>
-            <div class="weather">Температура:</div>
-          </div>
-          <div class="places__info">
-            <div class="name">Место 5</div>
-            <div class="descr">Здесь могла быть ваша реклама</div>
-            <div class="weather">Температура:</div>
+        </div>
+
+        <div class="col-12">
+          <div class="places">
+            <div class="places__info">
+              <div class="name">Место 1</div>
+              <div class="descr">Здесь могла быть ваша реклама</div>
+              <div class="weather">Температура:</div>
+            </div>
+            <div class="places__info">
+              <div class="name">Место 2</div>
+              <div class="descr">Здесь могла быть ваша реклама</div>
+              <div class="weather">Температура:</div>
+            </div>
+            <div class="places__info">
+              <div class="name">Место 3</div>
+              <div class="descr">Здесь могла быть ваша реклама</div>
+              <div class="weather">Температура:</div>
+            </div>
+            <div class="places__info">
+              <div class="name">Место 4</div>
+              <div class="descr">Здесь могла быть ваша реклама</div>
+              <div class="weather">Температура:</div>
+            </div>
+            <div class="places__info">
+              <div class="name">Место 5</div>
+              <div class="descr">Здесь могла быть ваша реклама</div>
+              <div class="weather">Температура:</div>
+            </div>
           </div>
         </div>
       </div>
@@ -75,7 +87,38 @@
 
 <script>
 export default {
-  name: "App",
+  data() {
+    return {
+      placeSearch: "",
+      buttonsList: [
+        { id: 0, name: "???" },
+        { id: 1, name: "???" },
+        { id: 2, name: "???" },
+        { id: 3, name: "???" },
+        { id: 4, name: "???" },
+      ],
+      coordsList: [
+        { id: 0, lat: "???", lon: "???" },
+        { id: 1, lat: "???", lon: "???" },
+        { id: 2, lat: "???", lon: "???" },
+        { id: 3, lat: "???", lon: "???" },
+        { id: 4, lat: "???", lon: "???" },
+      ],
+    };
+  },
+  methods: {
+    getCoords: async function () {
+      const keyAPI = "fc66e5ec568a2bd3c45c86a9b418c2c2";
+      console.log(keyAPI);
+      console.log(this.placeSearch);
+      const coordsURL =
+        "http://api.openweathermap.org/data/2.5/weather?q=${this.placeSearch}&appid=fc66e5ec568a2bd3c45c86a9b418c2c2";
+      const response = await fetch(coordsURL);
+      const data = await response.json();
+      console.log(data);
+      this.placeSearch = "";
+    },
+  },
 };
 </script>
 
